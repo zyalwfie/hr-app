48356da69049aa0c8780e6f971322a1180351edf
wafy <zyalwfie@gmail.com>
2025-12-26 23:33:33 +0800

    feat: set a page for 404 not found page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/css/app.js'])
+    <title>Page Not Found</title>
+</head>
+
+<body>
+    <div class="mx-auto flex min-h-dvh max-w-3xl flex-col items-center justify-center">
+        <div>
+            <header class="z-50 mb-auto flex w-full justify-center py-4">
+                <nav class="px-4 sm:px-6 lg:px-8">
+                    <a class="flex-none text-xl font-semibold sm:text-3xl dark:text-white" href="#"
+                        aria-label="Brand">Human Resources.</a>
+                </nav>
+            </header>
+
+            <main id="content">
+                <div class="px-4 py-10 text-center sm:px-6 lg:px-8">
+                    <h1 class="block text-7xl font-bold text-gray-800 sm:text-9xl dark:text-white">404</h1>
+                    <p class="mt-3 text-gray-600 dark:text-neutral-400">Oops, something went wrong.</p>
+                    <p class="text-gray-600 dark:text-neutral-400">Sorry, we couldn't find your page.</p>
+                    <div class="mt-5 flex flex-col items-center justify-center gap-2 sm:flex-row sm:gap-3">
+                        <a class="focus:outline-hidden inline-flex w-full items-center justify-center gap-x-2 rounded-lg border border-transparent bg-blue-600 px-4 py-3 text-sm font-medium text-white hover:bg-blue-700 focus:bg-blue-700 disabled:pointer-events-none disabled:opacity-50 sm:w-auto"
+                            href="{{ route('dashboard') }}">
+                            <svg class="size-4 shrink-0" xmlns="http://www.w3.org/2000/svg" width="24"
+                                height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round">
+                                <path d="m15 18-6-6 6-6" />
+                            </svg>
+                            Back to dashboard
+                        </a>
+                    </div>
+                </div>
+            </main>
+
+            <footer class="mt-auto py-5 text-center">
+                <div class="px-4 sm:px-6 lg:px-8">
+                    <p class="text-sm text-gray-500 dark:text-neutral-500">© All Rights Reserved. Human Resource 2026.
+                    </p>
+                </div>
+            </footer>
+        </div>
+    </div>
+
+</body>
+
+</html>
